e025cf37243c20bf9d8bf34b0daba7abd698b4f7
devmayank <devmayank@devmayank-PC>
2015-02-07 13:10:41 +0530

    Director class has been changed to serialized

---
+++ mobserver/src/com/rmr/model/Director.java
@@ -0,0 +1,41 @@
+package com.rmr.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.rmr.beans.FOSDetailsBean;
+
+public class Director implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String directorUsername;
+	private String directorPwd;
+	private List<FOSDetailsBean> fosList;
+
+	public String getDirectorUsername() {
+		return directorUsername;
+	}
+
+	public void setDirectorUsername(String directorUsername) {
+		this.directorUsername = directorUsername;
+	}
+
+	public String getDirectorPwd() {
+		return directorPwd;
+	}
+
+	public void setDirectorPwd(String directorPwd) {
+		this.directorPwd = directorPwd;
+	}
+
+	public List<FOSDetailsBean> getFosList() {
+		return fosList;
+	}
+
+	public void setFosList(List<FOSDetailsBean> fosList) {
+		this.fosList = fosList;
+	}
+
+}
