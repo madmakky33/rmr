703ef6185d1d4058dc65e68e433386ece47e342f
devmayank <devmayank@devmayank-PC>
2015-02-07 20:12:19 +0530

    uibiub

--- mobserver/src/com/rmr/model/Director.java
+++ mobserver/src/com/rmr/model/Director.java
@@ -9,6 +9,10 @@ public class Director implements Serializable {
 	/**
 	 * 
 	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 
+	 */
 	private String directorUsername;
 	private String directorPwd;
 	private List<FOSDetailsBean> fosList;
